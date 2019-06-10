1b2e096ec1ade66a1d5079cd114c4ceaf6c058e9
Simen Hornnes <simenhornnes@pc-104-26.eduroam.uib.no>
2019-06-10 15:28:14 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import FetchLocation from './components/FetchLocation';
+import UsersMap from './components/UsersMap';
+//import console = require('console');
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+//type Props = {};
+export default class App extends React.Component{
+  getUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+ //     console.log(position);
+    }, err => console.log(err));
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FetchLocation onGetLocation = {this.getUserLocationHandler} />
+        <UsersMap />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
