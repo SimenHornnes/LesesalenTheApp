9d88d5be088844a9e16788b4743a7a4774cfb419
Kyllingving <torjus.schaathun@hotmail.com>
2019-06-20 20:15:01 +0200

    Added Loading screen

--- components/SignUp.js
+++ components/SignUp.js
@@ -24,7 +24,7 @@ export default class SignUp extends React.Component {
                             marginBottom: 5
                         }}
                     >
-                        <Text style={{ color: 'pink', fontSize: 28, fontWeight: 'bold' }}>Sign Up</Text>
+                        <Text style={{ color: 'black', fontSize: 28, fontWeight: 'bold' }}>Sign Up</Text>
                     </View>
 
                     <Card>
@@ -60,7 +60,7 @@ export default class SignUp extends React.Component {
 
 const styles = StyleSheet.create({
     fullsize: {
-        backgroundColor: '#ADFF2F',
+        backgroundColor: '#77cde5',
         //flex: 1,
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
